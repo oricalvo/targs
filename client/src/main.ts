a3837aa9583c0f20147081059297b2a214c538d8
ori_c <ori.calvo@gmail.com>
2024-08-19 20:18:37 +0300

    Fix workspace

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,12 +1,12 @@
-
+import { readCliArgs } from "@oricalvo/targs/dist/reader";
+import { CliArgString } from "@oricalvo/targs/dist/types/string";
 
 function main() {
-    run({
-        build: Command({
-            verbose: Boolean(),
-        }),
-        rebuild: Command({
-            verbose: Boolean(),
-        }),
+    const cli = readCliArgs({
+        name: CliArgString(),
     });
+
+    console.log("Hello " + cli.name);
 }
+
+main();
